b88872827a94f98dc7f6e86dbd8e67bcf64b078e
Ji Younghyun <younghyun1@gmail.com>
2023-12-07 13:54:27 +0900

    comment cleanup

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,15 @@ use std::time::Instant;
 #[command(author, version, about, long_about = None)]
 struct Args {
     // Number of UUIDs to generate
-    #[arg(short, long, default_value_t = 1, help = "Specify the number of UUIDs to generate")]
+    #[arg(short, long, default_value_t = 1, help = "Specifies the number of UUIDs to generate.")]
     count: u32,
 
     // Format of the UUIDs
-    #[arg(short, long, default_value = "u", help = "Set the format of the UUIDs ('u' for bare, 'ul' for bare w. comma, 'q' for quoted, 'ql' for quoted w. comma, 'qlb' for [] brackets, 'qlbl' for {} brackets)")]
+    #[arg(short, long, default_value = "u", help = "Sets the format of the UUIDs. ('u' for bare, 'ul' for bare w. comma, 'q' for quoted, 'ql' for quoted w. comma, 'qlb' for [] brackets, 'qlbl' for {} brackets)")]
     format: String,
 
     // Output file
-    #[arg(short, long, help = "Specify an output file. Prints to stdout if not set")]
+    #[arg(short, long, help = "Specifies an output file. Prints to stdout if not set.")]
     output: Option<String>,
 
     // Check for duplicates
